51dbd822ce266a8ae4be5cce3d5157ddb456b9d4
rovast <rovast@163.com>
2019-05-28 15:44:22 +0800

    ✅ test package name

--- tests/Commands/BuildCommandTest.php
+++ tests/Commands/BuildCommandTest.php
@@ -1,8 +1,47 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Overtrue\PackageBuilder\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class BuildCommandTest extends TestCase
 {
+    /**
+     * @var Application
+     */
+    protected $application;
 
+    /**
+     * @var Command
+     */
+    protected $command;
+
+    /**
+     * @var CommandTester
+     */
+    protected $commandTester;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->application = new Application('Package Builder', '@package_version@');
+        $this->command = $this->application->find('build');
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    public function testEmptyPackageName()
+    {
+        $this->expectException(\Symfony\Component\Console\Exception\RuntimeException::class);
+        $this->commandTester->setInputs(['']);
+        $this->commandTester->execute(['command' => $this->command->getName()]);
+    }
+
+    public function testInvalidPackageName()
+    {
+        $this->expectException(\Symfony\Component\Console\Exception\RuntimeException::class);
+        $this->commandTester->setInputs(['vendor']);
+        $this->commandTester->execute(['command' => $this->command->getName()]);
+    }
 }
